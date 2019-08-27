da8b4aa8239542a0ac679709ff6073c6fc34033f
Yordan Ramchev <dakata911@gmail.com>
2019-08-27 18:37:10 +0300

    header/nav

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -2,23 +2,26 @@
   <header class="header">
     <div class="header__left">
       <div class="header__logo">
-        <nuxt-link :to="{name: 'index'}">
-          Logo
+        <nuxt-link :to="{ name: 'index' }">
+          <img src="http://www.beepurehoney.com/images/template/bee_pure_honey_logo.png" alt="" width="137">
         </nuxt-link>
       </div>
-      <div class="header__nav">
-        <TheNav />
-      </div>
+    </div>
+    <div class="header__center">
+      <TheNav />
     </div>
     <div class="header__right">
-      <a href="" @click.prevent="signOut">
-        sign out
-      </a>
-      <nuxt-link :to="{name: 'Cart'}">
-        ACCOUNT
+      <nuxt-link v-if="$store.getters['auth/isAuthenticated']" :to="{ name: 'Profile' }" class="header__account">
+        Profile
       </nuxt-link>
-      <nuxt-link :to="{name: 'Cart'}">
-        CART
+      <nuxt-link v-else :to="{ name: 'Login' }" class="header__account">
+        Login / Register
+      </nuxt-link>
+      <nuxt-link :to="{ name: 'Cart' }" class="header__cart">
+        <span class="icon-shopping-bag" />
+        <span class="header__cart-counter">
+          {{ cartCount }}
+        </span>
       </nuxt-link>
     </div>
   </header>
@@ -32,12 +35,9 @@ export default {
     TheNav
   },
 
-  methods: {
-    signOut () {
-      this.$store.dispatch('auth/signOut')
-        .then(() => {
-          this.$router.push({ name: 'index' })
-        })
+  computed: {
+    cartCount () {
+      return this.$store.getters['cart/count']
     }
   }
 }
@@ -49,11 +49,33 @@ export default {
     background: #fff;
     align-items: center;
     justify-content: space-between;
-    padding: 24px;
+    padding: 12px 24px;
+    border-bottom: 1px solid #f1f1f1;
   }
 
-  .header__left {
+  .header__left,
+  .header__right,
+  .header__cart {
     display: flex;
     align-items: center;
+  }
+
+  .icon-shopping-bag {
+    font-size: 24px;
+    margin-left: 12px;
+  }
+
+  .header__cart-counter {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+    font-weight: bold;
+    font-size: 13px;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background: #839366;
+    margin-left: 4px;
   }
 </style>
